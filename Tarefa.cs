9b8be2a13c5af3c7a54f9f3b2fbae48fae3c347c
Caualu <165745809+Caualu@users.noreply.github.com>
2024-09-13 18:10:51 -0300

    Add files via upload

---
+++ Tarefa.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoApi.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public bool Concluida { get; set; }
+       
+    }
+
+    
+}
---
+++ Valores.cs
@@ -0,0 +1,12 @@
+﻿namespace valor.Models
+{
+    public class Valores
+    {   public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public int Quantidade { get; set; }
+
+    }
+}
+
+
